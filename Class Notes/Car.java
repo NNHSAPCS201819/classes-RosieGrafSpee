c89cc42eeb99da138325f36a247cba4879667fa8
RosieGrafSpee <36432935+RosieGrafSpee@users.noreply.github.com>
2018-09-11 15:09:14 -0500

    nah
    nah

---
+++ Class Notes/Car.java
@@ -0,0 +1,55 @@
+
+/**
+ * This class models a car
+ *
+ * @author Rosiegrafspee
+ * @version 0.0.00
+ */
+public class Car
+{
+    /* instance variables - replace the example below with your own
+     * 
+     * specify the visibility of instance variables
+     * public : accessible by anyone
+     * private: only accessible by class. The entire one.
+     * 
+     * Don't forget to specify type and name!
+     */
+    private double fuelEff; // in units of liters per kilometer
+    private double fuelTank; // in units of liters
+
+    /**
+     * Constructor for objects of class Car
+     */
+    public Car()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    /*
+     * When defining a method, specify
+     *  The visibility
+     *  The return type
+     *  The method name
+     *  The parameters and their types
+     *
+     */
+    
+    /**
+     * Drives the car the specified distance, in kilometers, consuming fuel
+     * 
+     * @param distance the distance im km car drives
+     */
+}
