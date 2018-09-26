0403bf44640c18c9039a62f018e3ccf48d07f7ad
RosieGrafSpee <36432935+RosieGrafSpee@users.noreply.github.com>
2018-09-26 15:08:06 -0500

    aseasdfsd

--- Target/TargetComp.java
+++ Target/TargetComp.java
@@ -1,6 +1,7 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import javax.swing.JComponent;
+import java.util.Scanner;
 
 /**
  * Write a description of class TargetComp here.
@@ -13,8 +14,14 @@ public class TargetComp extends JComponent
     // instance variables - replace the example below with your own
     public void paint(Graphics g)
     {
+        System.out.print("Please input the radius of the innermost circle and the number of circles: ");
+        Scanner s = new Scanner(System.in);
+        
+        Strint text = s.nextLine();
+        text.
+        
         Graphics2D g_ = (Graphics2D) g;
-        Target tar = new Target(50, 4);
+        Target tar = new Target(x, y);
         tar.draw(g_);
     }
 }
